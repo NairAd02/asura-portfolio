4eb67e11b0f5df7d9e794ec7e8839d6d2ebe4c8f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-19 13:54:58 -0400

    fix(footer): update footer text to reflect creator's name

--- src/sections/root-layout/components/footer/footer.tsx
+++ src/sections/root-layout/components/footer/footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            © 2024 Adrian Suarez Padrón. Todos los derechos reservados.
+            Creado por Adrian Suarez Padrón.
           </p>
         </div>
         <div className="flex items-center space-x-4">
