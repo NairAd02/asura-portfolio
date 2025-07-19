43caabbabb869f72f72426677466a8574d6b79fb
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-19 12:29:08 -0400

    feat(skill-group-card): create SkillGroupCard component for displaying skill groups

---
+++ src/components/skill-group-card/skill-group-card.tsx
@@ -0,0 +1,36 @@
+import { SkillGroup } from "@/lib/types/skill-group";
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import AvatarContainer from "../ui/avatar-container";
+import { principalPlaceHolder } from "@/lib/place-holders";
+import { Badge } from "../ui/badge";
+
+interface Props {
+  skillGroup: SkillGroup;
+}
+
+export default function SkillGroupCard({ skillGroup }: Props) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2">
+          <AvatarContainer
+            image={skillGroup.icon || principalPlaceHolder}
+            fallback="s-g"
+          />
+          {skillGroup.title}
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <div className="space-y-2">
+          {skillGroup.skills.map((skill, index) => (
+            <div key={index} className="flex justify-between">
+              <span className="text-sm">{skill.name}</span>
+              <Badge variant="default">{skill.level.level}</Badge>
+            </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
