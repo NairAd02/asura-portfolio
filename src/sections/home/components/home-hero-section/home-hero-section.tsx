7addb3e65ac603a1d0f14c014f089e6ad0198ba2
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-19 15:59:10 -0400

    fix(home-hero-section): adjust padding and layout for improved responsiveness

--- src/sections/home/components/home-hero-section/home-hero-section.tsx
+++ src/sections/home/components/home-hero-section/home-hero-section.tsx
@@ -7,9 +7,9 @@ import React from "react";
 
 export default function HomeHeroSection() {
   return (
-    <section id="inicio" className="py-24 md:py-32">
+    <section id="inicio" className="p-8">
       <div className="container px-4 md:px-6">
-        <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+        <div className="flex flex-col-reverse md:flex-row justify-between gap-8">
           <div className="flex flex-col justify-center space-y-4">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
