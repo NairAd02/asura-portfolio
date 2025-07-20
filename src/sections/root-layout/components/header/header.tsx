079ef77862e025b7e36cc225884b463e0b8934f5
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-20 13:37:03 -0400

    feat(header): add MusicToggleButton to the header to control audio playback

--- src/sections/root-layout/components/header/header.tsx
+++ src/sections/root-layout/components/header/header.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import HeaderNav from "./header-nav/header-nav";
 import { useBreakpoint } from "@/hooks/screen/use-breakpoint";
 import { AlignCenter } from "lucide-react";
+import MusicToggleButton from "@/components/music-toggle-button/music-toggle-button";
 
 export default function Header() {
   const breakpoint = useBreakpoint();
@@ -13,14 +14,16 @@ export default function Header() {
     <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
       <nav>
         <div className="flex px-4 h-14 items-center justify-between">
+          <div className="flex items-center gap-2">
           <NavigationComponent href="/">
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-4">
               {" "}
               <AppLogo />
               <span className="font-bold">Asura-Portfolio</span>
             </div>
           </NavigationComponent>
-
+          <MusicToggleButton />
+          </div>
           {breakpoint === "xs" || breakpoint === "sm" || breakpoint === "md" || breakpoint === "lg" ? (
             <PopoverContainer trigger={<AlignCenter />}>
               <HeaderNav />
