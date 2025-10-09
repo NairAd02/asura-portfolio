4a630532ca178e1f6ed2a9763756e093c9a57a88
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-09 13:06:30 -0400

    feat(types): rename 'Proyect' to 'Project' and add new interfaces for project details and filters

--- src/lib/types/proyect.ts
+++ src/lib/types/proyect.ts
@@ -1,6 +1,6 @@
 import { Technology } from "./technologies";
 
-export interface Proyect {
+export interface Project {
   id: string;
   name: string;
   description: string;
@@ -13,4 +13,29 @@ export interface Proyect {
   impact: string;
   teachings: string;
   technologies: Technology[];
+}
+
+export interface ProjectDetails {
+  id: string;
+  name: string;
+  description: string;
+  mainImage?: string;
+  sourceCodeUrl?: string;
+  deploymentUrl?: string;
+  images: string[];
+  problem: string;
+  solution: string;
+  impact: string;
+  teachings: string;
+  technologies: Technology[];
+}
+
+export interface ProjectsFiltersDTO {
+  name?: string;
+  description?: string;
+  problem?: string;
+  solution?: string;
+  impact?: string;
+  teachings?: string;
+  technologies: string[];
 }
