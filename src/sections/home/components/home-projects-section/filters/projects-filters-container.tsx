3e22000c259138dcfba966cf3dfca94220abe1ce
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-10 11:49:35 -0400

    feat(projects-filters): implement ProjectsFiltersContainer and ProjectsFilters components for enhanced project filtering capabilities

---
+++ src/sections/home/components/home-projects-section/filters/projects-filters-container.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import useProjectsFilters from "./hooks/use-projects-filters";
+import ProjectsActiveFilters from "./components/projects-active-filters/projects-active-filters";
+import ProjectsFilters from "./projects-filters";
+import SheetContainer from "@/components/ui/sheet-container";
+import { Button } from "@/components/ui/button";
+import { SlidersHorizontal } from "lucide-react";
+import useTechnologies from "@/hooks/technologies/use-technologies";
+
+export default function ProjectsFiltersContainer() {
+  const {
+    filters,
+    getActiveFiltersCount,
+    handleChangeFilters,
+    handleResetFilters,
+  } = useProjectsFilters({});
+  const { technologies, loadingData: loadingDataTechs } = useTechnologies();
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-2">
+        <SheetContainer
+          title="Filtros de Proyectos"
+          trigger={
+            <Button className="flex items-center gap-2">
+              {" "}
+              <SlidersHorizontal /> Filtros
+            </Button>
+          }
+        >
+          <ProjectsFilters
+            filters={filters}
+            handleChangeFilters={handleChangeFilters}
+            technologies={{
+              data: technologies,
+              loading: loadingDataTechs,
+            }}
+          />
+        </SheetContainer>
+      </div>
+      {getActiveFiltersCount() > 0 && (
+        <ProjectsActiveFilters
+          filters={filters}
+          handleChangeFilters={handleChangeFilters}
+          getActiveFiltersCount={getActiveFiltersCount}
+          handleResetFilters={handleResetFilters}
+        />
+      )}
+    </div>
+  );
+}
