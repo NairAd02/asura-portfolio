67320c92a04f3377a9b570dc2fa5ee193dffa136
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-14 22:17:06 -0400

    feat(home-experience-section): integrate section filters into HomeExperienceSectionListContainer for dynamic experience retrieval

--- src/sections/home/components/home-experience-section/list/home-experience-section-list-container.tsx
+++ src/sections/home/components/home-experience-section/list/home-experience-section-list-container.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import { getExperiencesList } from "@/lib/services/experiences";
 import { Experience } from "@/lib/types/experiences";
 import HomeExperienceSectionList from "./home-experience-section-list";
+import { getSectionFilters } from "@/lib/cache/server-cache";
+import { paths } from "@/lib/routes/path";
+import { ExperiencesFilters } from "../filters/hooks/use-experiences-filters";
 
 export default async function HomeExperienceSectionListContainer() {
-  const res = await getExperiencesList({ technologies: [] });
+  const experiencesFilters = (await getSectionFilters<ExperiencesFilters>(
+    paths.home.experiencesSection
+  )) || {
+    technologies: [],
+  };
+
+  const res = await getExperiencesList(experiencesFilters);
 
   if (res.error) {
     console.log(res.error);
