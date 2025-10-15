99fbbd8ecbc9650015355cdb662f986d91ad5e01
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-14 22:17:53 -0400

    feat(home-experience-section): add ExperiencesFiltersContainer to HomeExperienceSectionList for improved filtering

--- src/sections/home/components/home-experience-section/list/home-experience-section-list.tsx
+++ src/sections/home/components/home-experience-section/list/home-experience-section-list.tsx
@@ -1,6 +1,7 @@
 import ExperienceCard from "@/components/experience-card/experience-card";
 import { Experience } from "@/lib/types/experiences";
 import React from "react";
+import ExperiencesFiltersContainer from "../filters/experiences-filters-container";
 
 interface Props {
   experiences: Experience[];
@@ -8,7 +9,8 @@ interface Props {
 
 export default function HomeExperienceSectionList({ experiences }: Props) {
   return (
-    <div className="mx-auto max-w-6xl py-12">
+    <div className="flex flex-col gap-6 mx-auto max-w-6xl py-12">
+      <ExperiencesFiltersContainer />
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         {experiences.map((experience, index) => (
           <ExperienceCard key={index} experience={experience} />
