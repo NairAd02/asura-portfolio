cb586879d7a1fdf1ba6967e22d3e494c4d8bf737
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-16 20:55:57 -0400

    feat(home-blogs-section): integrate BlogsFiltersContainer into HomeBlogsSectionList for improved filtering experience

--- src/sections/home/components/home-blogs-section/list/home-blogs-section-list.tsx
+++ src/sections/home/components/home-blogs-section/list/home-blogs-section-list.tsx
@@ -1,6 +1,7 @@
 import BlogCard from "@/components/blog-card/blog-card";
 import { Blog } from "@/lib/types/blogs";
 import React from "react";
+import BlogsFiltersContainer from "../filters/blogs-filters-container";
 
 interface Props {
   blogs: Blog[];
@@ -8,7 +9,8 @@ interface Props {
 
 export default function HomeBlogsSectionList({ blogs }: Props) {
   return (
-    <div className="mx-auto max-w-6xl py-12">
+    <div className="mx-auto max-w-6xl py-12 flex flex-col gap-6">
+      <BlogsFiltersContainer />
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         {blogs.map((blog, index) => (
           <BlogCard key={index} blog={blog} />
