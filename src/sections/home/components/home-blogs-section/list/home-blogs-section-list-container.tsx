84225b8829a018e39442cab39594e9e9f3df3233
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-16 20:56:03 -0400

    feat(home-blogs-section): update HomeBlogsSectionListContainer to utilize section filters for blog fetching

--- src/sections/home/components/home-blogs-section/list/home-blogs-section-list-container.tsx
+++ src/sections/home/components/home-blogs-section/list/home-blogs-section-list-container.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { getBlogsList } from "@/lib/services/blogs";
-import { Blog } from "@/lib/types/blogs";
+import { Blog, convertBlogsFiltersDTO } from "@/lib/types/blogs";
 import HomeBlogsSectionList from "./home-blogs-section-list";
+import { getSectionFilters } from "@/lib/cache/server-cache";
+import { BlogsFilters } from "../filters/hooks/use-blogs-filters";
+import { paths } from "@/lib/routes/path";
 
 export default async function HomeBlogsSectionListContainer() {
-  const res = await getBlogsList();
+  const blogsFilters =
+    (await getSectionFilters<BlogsFilters>(paths.home.blogsSection)) || {};
+  const res = await getBlogsList(convertBlogsFiltersDTO(blogsFilters));
 
   if (res.error) {
     console.log(res.error);
