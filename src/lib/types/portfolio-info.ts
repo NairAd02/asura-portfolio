8e9866c56957b0d6bbe96278b1b0d9d68926aea0
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-17 12:06:13 -0400

    feat(portfolio-info): add PortfolioInfo interface to define structure for portfolio data

---
+++ src/lib/types/portfolio-info.ts
@@ -0,0 +1,14 @@
+export interface PortfolioInfo {
+  id: string;
+  contact_name: string;
+  contact_image?: string;
+  introductory_phrase: string;
+  about_text: string;
+  feature_project_text: string;
+  work_experience_text: string;
+  technologies_and_skills_text: string;
+  education_and_certifications_text: string;
+  blog_and_post_text: string;
+  contact_text: string;
+  contact_email: string;
+}
