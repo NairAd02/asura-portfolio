107224d4ff06bc7f0958568361df556a6d57fdde
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-25 15:51:58 -0400

    feat(navigation-component): enhance NavigationComponent with new props for tab control and styling

--- src/components/navigation-component/navigation-component.tsx
+++ src/components/navigation-component/navigation-component.tsx
@@ -1,11 +1,32 @@
-import Link from "next/link"
-import React, { ReactNode } from "react"
+import Link from "next/link";
+import React, { ReactNode } from "react";
 
 interface Props {
-	children: ReactNode
-	href: string
+  children: ReactNode;
+  href: string;
+  inAnotherTab?: boolean;
+  className?: string;
+  scroll?: boolean;
 }
 
-export default function NavigationComponent({ children, href }: Props) {
-	return <Link href={href}>{children}</Link>
+export default function NavigationComponent({
+  children,
+  href,
+  inAnotherTab = false,
+  className,
+  scroll = true,
+}: Props) {
+  return (
+    <Link
+      href={href}
+      scroll={scroll}
+      {...(inAnotherTab && {
+        target: "_blank",
+        rel: "noopener noreferrer",
+      })}
+      className={className}
+    >
+      {children}
+    </Link>
+  );
 }
