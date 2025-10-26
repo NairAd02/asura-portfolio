8d0667ba995e4b13790d2b330f4dcaec0a2cbee3
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-26 00:17:15 -0400

    feat(project-details): implement dynamic metadata generation for project details page

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,9 +1,25 @@
+import { getProjectById } from "@/lib/services/projects";
 import ProjectDetailsContainer from "@/sections/projects/details/project-details-container";
+import { Metadata } from "next";
 import React from "react";
 
 type Props = {
   params: Promise<{ id: string }>;
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const perfumeId = (await params).id;
+  const res = await getProjectById(perfumeId);
+
+  if (!res.data || res.error)
+    throw new Error("Error al cargar al información del proyecto");
+
+  const project = res.data;
+  return {
+    title: project ? project.name : "Proyecto no encontrado",
+    description: project.description,
+  };
+}
 
 export default async function ProjectDetailsPage({ params }: Props) {
   const { id } = await params;
