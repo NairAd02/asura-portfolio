13d0e55077a214c9c2fc9942474dd7af55b752cb
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-26 17:59:45 -0400

    feat(home-experience-section): integrate experiences timeline component and adjust layout for responsive display

--- src/sections/home/components/home-experience-section/list/home-experience-section-list.tsx
+++ src/sections/home/components/home-experience-section/list/home-experience-section-list.tsx
@@ -1,7 +1,8 @@
-import ExperienceCard from "@/components/experience-card/experience-card";
 import { Experience } from "@/lib/types/experiences";
 import React from "react";
 import ExperiencesFiltersContainer from "../filters/experiences-filters-container";
+import ExperiencesTimeline from "./components/experiences-timeline";
+import ExperienceCard from "@/components/experience-card/experience-card";
 
 interface Props {
   experiences: Experience[];
@@ -11,7 +12,10 @@ export default function HomeExperienceSectionList({ experiences }: Props) {
   return (
     <div className="flex flex-col gap-6 mx-auto max-w-6xl py-12">
       <ExperiencesFiltersContainer />
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <div className="md:flex hidden pt-6">
+        <ExperiencesTimeline experiences={experiences} />
+      </div>
+      <div className="grid md:hidden grid-cols-1 lg:grid-cols-2 gap-4">
         {experiences.map((experience, index) => (
           <ExperienceCard key={index} experience={experience} />
         ))}
