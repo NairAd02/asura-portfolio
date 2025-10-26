6371bb593aea35881eaefb96290042421ca3e83b
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-25 23:21:17 -0400

    feat(skill-groups): enhance getSkillGroupsList to include technology details and mastery levels

--- src/lib/services/skill-groups.ts
+++ src/lib/services/skill-groups.ts
@@ -1,15 +1,25 @@
 "use server";
 
 import { createClient } from "../supabase/server";
+import { LevelEnum, MasteredTechnology } from "../types/mastered-technologies";
 import { SkillGroup } from "../types/skill-groups";
+import { Technology } from "../types/technologies";
 import { getImageUrlOrThrow } from "./supabase-storage";
 
 export async function getSkillGroupsList() {
   const supabase = await createClient();
-  const { data: skillGroupsData, error } = await supabase
-    .from("skillgroup")
-    .select("*");
-  const skillGroups = skillGroupsData as SkillGroup[];
+  const { data: skillGroupsData, error } = await supabase.from("skillgroup")
+    .select(`
+      *,
+      skillgroup_has_technology (
+        technology (*),
+        level
+      )
+    `);
+
+  const skillGroups = skillGroupsData as (SkillGroup & {
+    skillgroup_has_technology: { technology: Technology; level: LevelEnum }[];
+  })[];
 
   try {
     const skillGroupsMapped = await Promise.all(
@@ -19,13 +29,19 @@ export async function getSkillGroupsList() {
           icon: skillGroup.icon
             ? await getImageUrlOrThrow(supabase, skillGroup.icon)
             : undefined,
-          skills: await Promise.all(
-            skillGroup.skills.map(async (skill) => ({
-              ...skill,
-              icon: skill.icon
-                ? await getImageUrlOrThrow(supabase, skill.icon)
-                : undefined,
-            }))
+          masteredTechnologies: await Promise.all(
+            skillGroup.skillgroup_has_technology.map(
+              async (sht) =>
+                ({
+                  technology: {
+                    ...sht.technology,
+                    icon: sht.technology.icon
+                      ? await getImageUrlOrThrow(supabase, sht.technology.icon)
+                      : undefined,
+                  },
+                  level: sht.level,
+                } as MasteredTechnology)
+            )
           ),
         };
       })
