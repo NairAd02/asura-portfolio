a8296352f4d68bece939da64afad4bd6d74fc93b
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-28 15:22:54 -0400

    feat(home-blogs-section): integrate EmptyContent component to display a message when no blogs are available

--- src/sections/home/components/home-blogs-section/list/home-blogs-section-list.tsx
+++ src/sections/home/components/home-blogs-section/list/home-blogs-section-list.tsx
@@ -2,6 +2,7 @@ import BlogCard from "@/components/blog-card/blog-card";
 import { Blog } from "@/lib/types/blogs";
 import React from "react";
 import BlogsFiltersContainer from "../filters/blogs-filters-container";
+import EmptyContent from "@/components/empty-content/empty-content";
 
 interface Props {
   blogs: Blog[];
@@ -9,13 +10,22 @@ interface Props {
 
 export default function HomeBlogsSectionList({ blogs }: Props) {
   return (
-    <div className="mx-auto max-w-6xl py-12 flex flex-col gap-6">
-      <BlogsFiltersContainer />
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {blogs.map((blog, index) => (
-          <BlogCard key={index} blog={blog} />
-        ))}
-      </div>
+    <div className="mx-auto max-w-6xl py-12">
+      {blogs.length === 0 ? (
+        <EmptyContent
+          title="No he realizado ninguna publicación o blog de momento"
+          description=""
+        />
+      ) : (
+        <div className="flex flex-col gap-6">
+          <BlogsFiltersContainer />
+          <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {blogs.map((blog, index) => (
+              <BlogCard key={index} blog={blog} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
