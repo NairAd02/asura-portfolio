a81451698e390a1cf42635aa15d201d5d611d0de
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-30 12:29:20 -0400

    fix(home-projects-section): update grid layout for improved responsiveness and organization of project cards

--- src/sections/home/components/home-projects-section/list/home-projects-section-list.tsx
+++ src/sections/home/components/home-projects-section/list/home-projects-section-list.tsx
@@ -11,7 +11,7 @@ export default function HomeProjectsSectionList({ projects }: Props) {
   return (
     <div className="flex mt-10 flex-col mx-auto max-w-7xl gap-6">
       <ProjectsFiltersContainer />
-      <div className="mx-auto grid gap-6 lg:gap-4 w-full">
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
         {projects.map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
