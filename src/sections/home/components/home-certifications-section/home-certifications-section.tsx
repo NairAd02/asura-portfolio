723ea04300c98dab3982dd01118dff2fa60239f3
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-29 22:50:23 -0400

    fix(home-certifications-section): update section title to include 'Premios' for better clarity

--- src/sections/home/components/home-certifications-section/home-certifications-section.tsx
+++ src/sections/home/components/home-certifications-section/home-certifications-section.tsx
@@ -16,7 +16,7 @@ export default function HomeCertificationsSection({
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl text-primary font-bold tracking-tighter sm:text-4xl md:text-5xl">
-              Educación y Certificaciones
+              Educación, Premios y Certificaciones
             </h2>
             <p className="max-w-[900px] text-black font-semibold md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               {certificationsSectionInfo.education_and_certifications_text}
