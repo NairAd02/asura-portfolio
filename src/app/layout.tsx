af1ea985fd1e2248aa0cdcc1408383c8956b7de3
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-26 23:25:24 -0500

    feat(layout): implement dynamic metadata generation using personal information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import Header from "@/sections/root-layout/components/header/header";
 import Footer from "@/sections/root-layout/components/footer/footer";
 import ProgressBar from "@/components/providers/progress-bar.";
 import { Lora, Cormorant_Garamond } from "next/font/google";
+import { getPersonalInformationInfo } from "@/lib/services/portfolio-info";
+import { principalPlaceHolder } from "@/lib/place-holders";
 
 const lora = Lora({ subsets: ["latin"], variable: "--font-lora" });
 const cormorant = Cormorant_Garamond({
@@ -11,10 +13,39 @@ const cormorant = Cormorant_Garamond({
   variable: "--font-cormorant",
 });
 
-export const metadata: Metadata = {
-  title: "Asura-Portfolio",
-  description: "Portafolio personalizado para mostrar mis habilidades",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const res = await getPersonalInformationInfo();
+
+  if (!res.data || res.error)
+    throw new Error("Error al cargar la información personal");
+
+  const personalInformation = res.data;
+  return {
+    title:
+      "Hola, soy: " +
+      personalInformation.contact_name +
+      ". Y este es mi Portafolio",
+    description: personalInformation.introductory_phrase,
+    openGraph: {
+      title:
+        "Hola, soy: " +
+        personalInformation.contact_name +
+        ". Y este es mi Portafolio",
+      description: personalInformation.introductory_phrase,
+      images: [personalInformation.contact_image || principalPlaceHolder],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title:
+        "Hola, soy: " +
+        personalInformation.contact_name +
+        ". Y este es mi Portafolio",
+      description: personalInformation.introductory_phrase,
+      images: [personalInformation.contact_image || principalPlaceHolder],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
